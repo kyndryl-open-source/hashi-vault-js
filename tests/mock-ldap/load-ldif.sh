d77d38a28acee7dffb850e48208b2c6c7866dadc
Rod Anami <rod.anami@br.ibm.com>
2022-07-05 10:21:23 -0300

    Added active directory schema

--- tests/mock-ldap/load-ldif.sh
+++ tests/mock-ldap/load-ldif.sh
@@ -2,9 +2,12 @@
 
 source ldap.env
 
+# Configure schema with AD attributes
+cat config-schema.ldif | docker exec -i ck-ldap ldapmodify -Y EXTERNAL -H ldapi:///
+docker exec -i ck-ldap ldapsearch -Y EXTERNAL -Q -H ldapi:/// -LLL -o ldif-wrap=no -b cn=config '(olcAttributeTypes=*)' | grep MSDN
+# Check anonymous connection
 ldapwhoami -H $LDAP_URL -x
+# Add dummy entities for testing
 ldapadd -x -D "cn=admin,dc=chatopsknight,dc=com" -w $LDAP_ADMIN_PASSWORD -H $LDAP_URL -f chatops-knight.ldif
 ldapsearch -x -D "cn=admin,dc=chatopsknight,dc=com" -w $LDAP_ADMIN_PASSWORD -H $LDAP_URL -LLL -b "dc=chatopsknight, dc=com" -s sub "(objectclass=*)"
-
-
-#ldapmodify -xvvv  -D "cn=admin,cn=config" -w $LDAP_CONFIG_PASSWORD -H $LDAP_URL -f ck-permissions.ldif
+ldapsearch -x -D "cn=admin,dc=chatopsknight,dc=com" -w $LDAP_ADMIN_PASSWORD -H $LDAP_URL -LLL -b "dc=chatopsknight, dc=com" -s sub "(userPrincipalName=rod.anami@chatopsknight.com)"
