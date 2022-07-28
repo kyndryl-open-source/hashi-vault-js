e24703ce7f439c2617884d5146c8fe98f20ecc74
phr3nzy <adilosama47@gmail.com>
2022-07-28 12:55:58 +0200

    add comments to VaultConfig values

--- Vault.d.ts
+++ Vault.d.ts
@@ -4,18 +4,54 @@
 
 /// <reference types="node" />
 
-import type { Axios, AxiosProxyConfig } from "axios"
+import type { Axios, AxiosProxyConfig } from "axios";
 import type { Agent } from "https";
 
 export type VaultConfig = {
+	/**
+	 * Indicates if the HTTP request to the Vault server should use
+	 * HTTPS (secure) or HTTP (non-secure) protocol
+	 */
 	https?: boolean;
+	/**
+	 * If https is true, then provide client certificate, client key and
+   * the root CA cert.
+   * Client cert and key are optional now.
+	 * @example './path/to/your/cert.crt'
+	 */
 	cert: string;
+	/**
+	 * @example './path/to/your/key.key
+	 */
 	key: string;
+	/**
+	 * @example './path/to/your/cacert.crt
+	 */
 	cacert: string;
+	/**
+	 * Indicate the server name/IP, port and API version for the Vault instance,
+   * all paths are relative to this one
+	 */
 	baseUrl?: string;
+	/**
+	 * Sets the root path after the base URL, it translates to a
+   * partition inside the Vault where the secret engine / auth method was enabled.
+   * Can be passed individually on each function through mount parameter
+	 */
 	rootPath: string;
-	timeout: string | number;
-	proxy?: AxiosProxyConfig;
+	/**
+	 * HTTP request timeout in milliseconds
+	 */
+	timeout: number;
+	/**
+	 * If should use a proxy or not by the HTTP request
+   * Example:
+   * proxy: { host: proxy.ip, port: proxy.port }
+	 */
+	proxy?: AxiosProxyConfig | boolean;
+	/**
+	 * Namespace (multi-tenancy) feature available on all Vault Enterprise versions
+	 */
 	namespace?: string;
 }
 
@@ -923,7 +959,7 @@ declare module "hashi-vault-js" {
 		private baseUrl: string;
 		private rootPath: string;
 		private timeout: string | number;
-		private proxy: AxiosProxyConfig;
+		private proxy: AxiosProxyConfig | boolean;
 		private namespace: string;
 		private instance: Axios;
 		private agent: Agent | boolean;
