44a2cdc2474acb5df0c6cff4635fd740447118fb
Rod Anami <rod.anami@kyndryl.com>
2025-04-16 09:43:04 -0300

    refactored typescript unit test Signed-off-by: Rod Anami <rod.anami@kyndryl.com>

--- dist/Vault.d.ts
+++ dist/Vault.d.ts
@@ -1,4 +1,3 @@
-export = Vault;
 /**
 * @constructor
 * @param {boolean} [params.https=false]
@@ -11,7 +10,7 @@ export = Vault;
 * @param {boolean} [params.proxy=false]
 * @param {string} [params.namespace]
 */
-declare class Vault {
+export default class Vault {
     constructor(params: any);
     /** @type {boolean} */
     https: boolean;
@@ -1323,17 +1322,17 @@ declare class Vault {
     * @param {string} name
     * @param {Object} params
     * @param {boolean} params.generate
-    * @param {boolean} [params.exported]
+    * @param {boolean} [params.exported] - whether the key is exportable as QR code
     * @param {number} [params.key_size=20]
     * @param {string} [params.key_url]
     * @param {string} [params.key]
-    * @param {string} [params.issuer]
+    * @param {string} [params.issuer] - key issuing entity
     * @param {string} [params.account_name]
-    * @param {number} [params.period]
-    * @param {string} [params.algorithm]
-    * @param {number} [params.digits]
-    * @param {number} [params.skew]
-    * @param {number} [params.gr_size]
+    * @param {number} [params.period=30] - length of time for the counter on the code calculation
+    * @param {string} [params.algorithm=sha1] - code generator algorithm, either "SHA1", "SHA256", or "SHA512"
+    * @param {number} [params.digits] - number of code digits, either 6 or 8
+    * @param {number} [params.skew=1] - number of delay periods valid for code validation, either 0 or 1
+    * @param {number} [params.gr_size=200] - pixel size of the QR code image
     * @param {string} [mount]
     * @returns {PromiseLike<Object>}
     */
@@ -1351,5 +1350,12 @@ declare class Vault {
         skew?: number;
         gr_size?: number;
     }, mount?: string): PromiseLike<any>;
+    /**
+    * @param {string} token
+    * @param {string} name
+    * @param {string} [mount]
+    * @returns {PromiseLike<Object>}
+    */
+    readTOTPKey(token: string, name: string, mount?: string): PromiseLike<any>;
 }
 //# sourceMappingURL=Vault.d.ts.map
